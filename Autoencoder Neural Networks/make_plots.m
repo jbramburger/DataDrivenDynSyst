36c2efbe2c7ab0b36b9c1b2206437267d0977c7c
Jason Bramburger <68243199+jbramburger@users.noreply.github.com>
2022-10-14 11:53:30 -0400

    Add files via upload

--- Autoencoder Neural Networks/make_plots.m
+++ Autoencoder Neural Networks/make_plots.m
@@ -49,6 +49,33 @@ legend('$\rho(x)$','$h^{-1}(x)$','Interpreter','Latex','Location','Best','FontSi
 set(gca,'FontSize',16,'Xlim',[0 1],'Ylim',[0 1])
 box on
 
+%% Tent --> Sine Conjugacy results 
+%   Date comes from Tent2Sine.ipynb notebook
+
+load Tent2Sine_Conj.mat 
+% --> Outputs are (x,y,xdecode)
+%        x = linspace(0,1,1000)
+%        y = encoder(x) - approximate conjugacy output
+%        xdecode = decoder(x) - approximate inverse conjugacy output
+
+% Plot encoder 
+figure(1)
+hold on
+plot(x,y,'Color',[36/255 122/255 254/255],'LineWidth',6)
+xlabel('$x$','Interpreter','Latex')
+ylabel('$y$','Interpreter','Latex')
+set(gca,'FontSize',16,'Xlim',[0 1],'Ylim',[-1 0])
+box on
+
+% Plot decoder
+figure(2)
+hold on
+plot(-x,xdecode,'Color',[1 69/255 79/255],'LineWidth',6)
+xlabel('$y$','Interpreter','Latex')
+ylabel('$x$','Interpreter','Latex')
+set(gca,'FontSize',16,'Xlim',[-1 0],'Ylim',[0 1])
+box on
+
 
 
 
