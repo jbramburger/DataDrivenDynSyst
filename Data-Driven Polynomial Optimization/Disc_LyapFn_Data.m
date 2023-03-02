df6e3ab212a7a3c446fe3cd66cb902474a8ef78b
Jason Bramburger <68243199+jbramburger@users.noreply.github.com>
2023-03-02 12:32:04 -0500

    Add files via upload

---
+++ Data-Driven Polynomial Optimization/Disc_LyapFn_Data.m
@@ -0,0 +1,142 @@
+% -------------------------------------------------------------------------
+% Identifying Lyapunov Functions from Data with EDMD
+
+% The goal of this script is to use EDMD and semidefinite programming to 
+% identify a Lyapunov function for the planar discrete-time system
+%      x1 --> 0.3*x1 , 
+%      x2 --> -x1 + 0.5*x2 + (7/18)*x1^2,
+% using only data gathered from the system.
+% 
+% To run this notebook one requires paths to the freely available software
+% packages YALMIP and MOSEK. YALMIP can be downloaded at:
+%           https://yalmip.github.io/download/
+% and MOSEK can be downloaded at:
+%           https://www.mosek.com/downloads/
+%
+% Written by J. Bramburger and G. Fantuzzi.
+%
+% -------------------------------------------------------------------------
+
+% Clean workspace
+clear; close all; clc
+yalmip clear
+format long
+
+%% Method Parameters 
+% maxDp = max degree of Dp dictionary of obserables
+% maxDq = max degree of Dq dictionary of obserables
+% epsilon = hyperparameter specific to Lyapunov function for sharp bounds
+maxDp = 4;
+maxDq = 6;
+epsilon = 1;
+
+%% Generate synthetic data
+
+% Number of data points
+N = 1e2; 
+
+% Random points in [-2,2]x[-2,2]
+xdat = 2*rand(N,2) - 1;
+ydat = zeros(N,2);
+
+% Images of random points under map dynamics
+ydat(:,1) = 0.3*xdat(:,1);
+ydat(:,2) = -xdat(:,1) + 0.5*xdat(:,2) + (7/18)*xdat(:,1).^2;
+
+%% Create P and Q matrices
+
+% Q matrix
+pow = monpowers(2,maxDq);
+ell = size(pow,1); % number of nontrivial monomials in Q
+Q = zeros(ell,N);
+for i = 1:ell
+   zx = xdat.^pow(i,:);
+   Q(i,:) = prod(zx,2);
+end
+
+% P matrix
+pow = monpowers(2,maxDp);
+m = size(pow,1); % number of nontrivial monomials in P
+P = zeros(m,N);
+for i = 1:m
+   zy = ydat.^pow(i,:);
+   P(i,:) = prod(zy,2)';
+end
+
+%% Create Koopman and Lie derivative matrix approximations
+
+% Koopman approximation
+K = P*pinv(Q);
+
+% Symbolic variables
+x = sdpvar(2,1); % 2D state variable
+z = monolist(x,maxDp,0); % monomials that make up span(Dp)
+w = monolist(x,maxDq,0); % monomials that make up span(Dq)
+c = sdpvar(length(z),1); % coeffs to build Lyapunov function in span(Dp)
+
+% Lie approximation
+% ---> Lyapunov function = c.'*z in span(Dp)
+L = c.'*(K*w) - c.'*z;
+
+%% Identify Lyapunov function with SOS programming
+
+% Inequality constraints posed relaxed to SOS conditions
+cons = [sos(c.'*z - epsilon*dot(x,x)); sos(-L - epsilon*dot(x,x))];
+
+% Set solver to MOSEK
+opts = sdpsettings;
+opts.solver = 'mosek';
+
+% Objective function: minimize l^1 norm of coefficients
+OBJ = sum(abs(c));
+
+% Solve SOS problem
+solvesos(cons,OBJ,opts,c)
+
+% Print coefficients after removing those smaller than 10^-4 
+c = clean(value(c), 1e-4)
+
+%% Check identified Lyapunov function is a real Lyapunov function
+% ---> This time we maximize epsilon and check if it is positive
+
+% Initializations
+yalmip clear
+x = sdpvar(2,1); % 2D state variable
+z = monolist(x,maxDp,0); % monomials that make up span(Dp)
+sdpvar epsilon % epsilon is now a variable to be maximized
+
+% Discovered Lyapunov function
+v = c.'*z;
+
+% True right-hand-side of map
+f = [0.3*x(1); -x(1) + 0.5*x(2) + (7/18)*x(1)^2];
+
+% True Lie derivative
+Lie_exact = replace(v,x,f);
+
+% Find Lyapunov function
+solvesos([sos(v - epsilon*dot(x,x)); sos(- (Lie_exact - v) - epsilon*dot(x,x))],-epsilon)
+fprintf('Maximized value of epsilon is: %f \n',value(epsilon))
+
+if value(epsilon) > 0
+   fprintf('We have discovered a true Lyapunov function from the data! \n') 
+else
+    fprintf('Something went wrong - we cannot verify that this is a Lyapunov function. \n')
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Data-Driven Polynomial Optimization/MG_LyapFn.m
@@ -0,0 +1,98 @@
+% -------------------------------------------------------------------------
+% Identifying Lyapunov Functions with Semidefinite Programming
+%
+% The goal of this script is to use semidefinite programming to identify a
+% Lyapunov function for the planar Moore-Greitzer system
+%      x1' = -x2 - 1.5x1^2 - 0.5x1^3, 
+%      x2' = 3x1 - x2.
+% The script expands the unknown Lyapunov function as a polynomial up to a
+% given degree and tunes the coefficients in order to satisfy both of the
+% constraints:
+%      V - x1^2 - x2^2 is SOS
+%      -LV - x1^2 - x1^2 is SOS
+% where LV is the Lie derivative associated to the Moore-Greitzer system.
+%
+% To run this notebook one requires paths to the freely available software
+% packages YALMIP and MOSEK. YALMIP can be downloaded at:
+%           https://yalmip.github.io/download/
+% and MOSEK can be downloaded at:
+%           https://www.mosek.com/downloads/
+%
+% This script accompanies Section 4.2 of Data-Driven Methods for
+% Dynamic Systems. 
+%
+% Author: Jason J. Bramburger
+% -------------------------------------------------------------------------
+
+% Clean workspace
+clear all; 
+close all;
+yalmip clear
+clc
+
+%% Discovering polynomial Lyapunov function
+
+% Degree of Lyapunov function
+degV = 6;
+
+% Symbolic variables
+x = sdpvar(2,1);
+
+% Moore-Greitzer right-hand-side
+f = [-x(2) - 1.5*x(1)^2 - 0.5*x(1)^3; 3*x(1) - x(2)];
+
+% Lyapunov function, gradient, and Lie derivative
+[v,vc,vm] = polynomial(x,degV,1); 
+Dv = jacobian(v,x);        
+Lie = dot(Dv,f);
+
+% Find Lyapunov function
+cons = [sos(v - dot(x,x)); sos(-Lie - dot(x,x))]; % SOS constraints to be satisfied 
+obj = sum(abs(vc)); % objective function: minimize l^1 norm of coefficients 
+sol = solvesos(cons,obj,[],vc);
+
+%% Print Results
+
+% Clean window
+clc
+
+% Print if solution was solved
+if sol.problem == 0 % <-- solution is solved successfully
+    
+    % Remove out small values from coefficients
+    coeffs = clean(value(vc),1e-5);
+    
+    % Display Lyapunov function
+    fprintf('Lyapunov function identified! \n')
+    fprintf('\n')
+    fprintf('V(x(1),x(2)) = ') 
+    sdisplay(coeffs.'*vm)
+else
+    % If no Lyapunov function, tell the user
+    fprintf('Sorry, unable to identify a Lyapunov function \n')
+end
+
+%% Plot Lyapunov function
+
+% Spatial coordinates
+N = 1000;
+x2 = linspace(-5,5,N);
+y2 = linspace(-5,5,N);
+[X,Y] = meshgrid(x2,y2);
+
+% Build Lyapunov function
+[pow, coeff] = getexponentbase(v,x);
+lyap = zeros(size(X));
+for i = 1:numel(lyap)
+    lyap(i) = lyap(i) + coeff*( X(i).^pow(:,1) .* Y(i).^pow(:,2) );
+end
+
+% Surface plot
+surf(X,Y,lyap)
+shading interp
+xlabel('$x_1$','Interpreter','Latex')
+ylabel('$x_2$','Interpreter','Latex')
+zlabel('$V(x_1,x_2)$','Interpreter','Latex')
+set(gca,'FontSize',16,'Xlim',[-5 5],'Ylim',[-5 5])
+
+
